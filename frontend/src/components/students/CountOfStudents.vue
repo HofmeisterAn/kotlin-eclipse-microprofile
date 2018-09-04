27796fab5166701a98570d4f431fc0e65f25c72f
Andre Hofmeister <sw.padme.amidala@icloud.com>
2018-09-04 21:53:44 +0200

    Basic implementation of CountOfStudents component, we need to clean this up.

---
+++ frontend/src/components/students/CountOfStudents.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span class="emphasis">{{ count }}</span>
+    dadasd
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    props: {
+      count22: Number
+    },
+    data() {
+        return {
+            count: 9
+        }
+    },
+    methods: {
+        foo(): string { return "foo"; },
+        bar(): string { return "bar"; }
+    },
+    computed: {
+        fooBar(): string {
+            return this.foo() + this.bar();
+        }
+    }
+});
+</script>
+
+<style>
+.emphasis {
+  color: red;
+}
+</style>
