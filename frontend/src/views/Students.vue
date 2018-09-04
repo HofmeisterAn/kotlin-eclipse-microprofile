72cbce7e7817b3784d791da9e71a046aeebc4827
Andre Hofmeister <sw.padme.amidala@icloud.com>
2018-09-04 21:53:44 +0200

    Basic implementation of CountOfStudents component, we need to clean this up.

---
+++ frontend/src/views/Students.vue
@@ -0,0 +1,14 @@
+<template>
+  <CountOfStudents />
+</template>
+
+<script>
+import CountOfStudents from '@/components/students/CountOfStudents.vue'
+
+export default {
+  name: 'students',
+  components: {
+    CountOfStudents
+  }
+}
+</script>
