e2cfdb9e7bc03c28272d63a3445c642cc5488f6c
Andre Hofmeister <sw.padme.amidala@icloud.com>
2018-10-10 21:29:21 +0200

    Implemented example repository for entity student.

--- backend/src/main/kotlin/com/andrehofmeister/schoolhouse/student/StudentRepositoryImpl.kt
+++ backend/src/main/kotlin/com/andrehofmeister/schoolhouse/student/StudentRepositoryImpl.kt
@@ -1,26 +1,30 @@
 package com.andrehofmeister.schoolhouse.student
 
 import java.util.UUID
+import javax.transaction.Transactional
 
 @DefaultStudentRepository
-internal class StudentRepositoryImpl : StudentRepository() {
+internal open class StudentRepositoryImpl : StudentRepository() {
   override fun all(): List<Student> {
     return entityManager.createQuery("select s from Student s", Student::class.java).resultList.toList()
   }
 
   override fun get(id: UUID): Student {
-    TODO("not implemented")
+    return entityManager.find(Student::class.java, id) ?: Student.default
   }
 
+  @Transactional
   override fun create(obj: Student) {
-    TODO("not implemented")
+    entityManager.persist(obj)
   }
 
+  @Transactional
   override fun update(obj: Student) {
-    TODO("not implemented")
+    entityManager.merge(obj)
   }
 
+  @Transactional
   override fun delete(obj: Student) {
-    TODO("not implemented")
+    entityManager.remove(if (entityManager.contains(obj)) obj else entityManager.merge(obj))
   }
 }
--- backend/src/test/kotlin/com/andrehofmeister/schoolhouse/student/StudentTest.kt
+++ backend/src/test/kotlin/com/andrehofmeister/schoolhouse/student/StudentTest.kt
@@ -5,9 +5,14 @@ import org.jboss.arquillian.junit.Arquillian
 import org.jboss.shrinkwrap.api.ShrinkWrap
 import org.jboss.shrinkwrap.api.gradle.archive.importer.embedded.EmbeddedGradleImporter
 import org.jboss.shrinkwrap.api.spec.WebArchive
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.Date
+import java.util.UUID
 import javax.inject.Inject
 
 @RunWith(Arquillian::class)
@@ -23,8 +28,46 @@ internal class StudentTest {
   @field:[Inject DefaultStudentRepository]
   private lateinit var studentRepository: StudentRepository
 
+  private lateinit var student: Student
+
+  @Before
+  fun before() {
+    student = Student(UUID(0, 0), "Foo", "Bar", Date())
+  }
+
   @Test
   fun simple_test_without_any_purpose_except_testing_dependency_injection() {
     assertTrue(studentRepository.all().isEmpty())
+  }
+
+  @Test
+  fun student_repository_create_record() {
+    // Given is a new student with default id
+    // When a new student record is created
+    studentRepository.create(student)
+    // Then a unique id gets generated for the student
+    assertNotEquals(Student.default.id, student.id)
+  }
+
+  @Test
+  fun student_repository_update_record() {
+    // Given is a new student with default name
+    studentRepository.create(student)
+    // When his or her name changes
+    student.firstName = "Bar"
+    student.lastName = "Foo"
+    studentRepository.update(student)
+    // Then his or her new name is stored in the database
+    assertEquals("Bar", studentRepository.get(student.id).firstName)
+  }
+
+  @Test
+  fun student_repository_delete_record() {
+    // Given is a student
+    studentRepository.create(student)
+    // When he or she leaves the college
+    studentRepository.delete(student)
+    // Then his or her database record gets deleted
+    assertEquals(Student.default, studentRepository.get(student.id))
   }
 }
