1c633ddddbab223a96666821ed89e6f99380fe1c
Andre Hofmeister <sw.padme.amidala@icloud.com>
2018-10-17 21:45:00 +0200

    Started to add an example implementation of jwt authentication.

--- backend/src/main/kotlin/com/andrehofmeister/schoolhouse/core/MyApplication.kt
+++ backend/src/main/kotlin/com/andrehofmeister/schoolhouse/core/MyApplication.kt
@@ -1,7 +1,9 @@
 package com.andrehofmeister.schoolhouse.core
 
+import org.eclipse.microprofile.auth.LoginConfig
 import javax.ws.rs.ApplicationPath
 import javax.ws.rs.core.Application
 
+@LoginConfig(authMethod = "MP-JWT", realmName = "TCK-MP-JWT")
 @ApplicationPath("/rest")
 class MyApplication : Application()
--- backend/src/main/kotlin/com/andrehofmeister/schoolhouse/student/StudentController.kt
+++ backend/src/main/kotlin/com/andrehofmeister/schoolhouse/student/StudentController.kt
@@ -1,5 +1,6 @@
 package com.andrehofmeister.schoolhouse.student
 
+import org.eclipse.microprofile.jwt.JsonWebToken
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
 import javax.ws.rs.GET
@@ -8,6 +9,9 @@ import javax.ws.rs.Path
 @ApplicationScoped
 @Path("/api/students")
 open class StudentController {
+  @field:[Inject]
+  private lateinit var callerPrincipal: JsonWebToken
+
   @field:[Inject DefaultStudentService]
   private lateinit var studentService: StudentService
 
