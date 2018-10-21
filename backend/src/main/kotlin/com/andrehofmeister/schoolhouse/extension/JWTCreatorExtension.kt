d6dd21ff658f666c15e15c34f193ad57a4599425
Andre Hofmeister <sw.padme.amidala@icloud.com>
2018-10-21 10:05:53 +0200

    Added test for expired tokens.

--- backend/src/main/kotlin/com/andrehofmeister/schoolhouse/extension/JWTCreatorExtension.kt
+++ backend/src/main/kotlin/com/andrehofmeister/schoolhouse/extension/JWTCreatorExtension.kt
@@ -1,9 +1,11 @@
 package com.andrehofmeister.schoolhouse.extension
 
 import com.auth0.jwt.JWTCreator
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.Date
 
-fun JWTCreator.Builder.withPayload(payloadClaims: HashMap<String, Any>): JWTCreator.Builder {
+fun JWTCreator.Builder.withPayload(payloadClaims: Map<String, Any>): JWTCreator.Builder {
   payloadClaims.forEach { k, v ->
     when (v) {
       is Int -> withClaim(k, v)
@@ -12,6 +14,7 @@ fun JWTCreator.Builder.withPayload(payloadClaims: HashMap<String, Any>): JWTCrea
       is Boolean -> withClaim(k, v)
       is String -> withClaim(k, v)
       is Date -> withClaim(k, v)
+      is LocalDateTime -> withClaim(k, Date.from(v.atZone(ZoneId.systemDefault()).toInstant()))
     }
   }
 
